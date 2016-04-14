5c5b34903d974589489f1f3572efad5d11c688fc
MatthewAllanWilson <mattwils@gmail.com>
2016-04-14 16:35:44 -0700

    handlebars added and css files scaffolding set

--- js/app.js
+++ js/app.js
@@ -8,16 +8,22 @@ function Post (obj){
 }
 
 Post.prototype.toHtml = function() {
-  var $newPortfolioPiece = $('article.template').clone();
-  $newPortfolioPiece.removeClass('template');
-  $newPortfolioPiece.find('.title').html(this.title);
-  $newPortfolioPiece.find('.date').html(this.date);
-  $newPortfolioPiece.find('.description').html(this.description);
-  $newPortfolioPiece.find('.link').html(this.link);
-  $newPortfolioPiece.append('<hr>');
-
-  return $newPortfolioPiece;
+  var $source = $('#project-template').html();
+  var template = Handlebars.compile($source);
+  return template(this);
 };
+
+// Post.prototype.toHtml = function() {
+//   var $newPortfolioPiece = $('article.template').clone();
+//   $newPortfolioPiece.removeClass('template');
+//   $newPortfolioPiece.find('.title').html(this.title);
+//   $newPortfolioPiece.find('.date').html(this.date);
+//   $newPortfolioPiece.find('.description').html(this.description);
+//   $newPortfolioPiece.find('.link').html(this.link);
+//   $newPortfolioPiece.append('<hr>');
+//
+//   return $newPortfolioPiece;
+// };
 
 portfolioData.forEach(function(ele) {
   posts.push(new Post(ele));
