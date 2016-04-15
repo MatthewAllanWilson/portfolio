1468ff9937f530c7ed06c808d1b7ee857bfe3fd4
MatthewAllanWilson <mattwils@gmail.com>
2016-04-14 19:48:15 -0700

    just a little experimenting with CSS

--- js/view.js
+++ js/view.js
@@ -2,7 +2,6 @@ var projectView = {};
 
 projectView.handleMainNav = function() {
   $('.main-nav').on('click', '.tab', function(){
-    console.log($(this).data('content'));
     $('#about').hide();
     $('#main-page').hide();
     $("#" + $(this).data('content')).fadeIn();
