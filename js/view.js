1c9ee9395e6121496949bb4f6f6472cfe94fc09c
MatthewAllanWilson <mattwils@gmail.com>
2016-04-15 17:53:53 -0700

    finally got it working as before

--- js/view.js
+++ js/view.js
@@ -2,8 +2,11 @@ var projectView = {};
 
 projectView.handleMainNav = function() {
   $('.main-nav').on('click', '.tab', function(){
-    // $('#about').hide();
-    $('.whole-page').hide();
+    //I want this to hide everything on the page initially, then display only the sections that have been selected, including a home papge option that has the entire page selected.  However, I can only get it to work when I turn off all the individual sections.
+    // $('#whole-page').hide();
+    $('#main-page').hide();
+    $('#about').hide();
+    $('#projects').hide();
     $('#' + $(this).data('content')).fadeIn();
     console.log($(this).data('content'));
   });
