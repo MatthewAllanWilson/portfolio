3984bf2797426bd28ed6219d125a3fa5a73ee435
MatthewAllanWilson <mattwils@gmail.com>
2016-04-20 15:48:35 -0700

    got local storage working again but with eTag turned off

--- js/app.js
+++ js/app.js
@@ -25,20 +25,21 @@
   };
 
   Post.fetchAll = function (){
-    if (localStorage.projects){
-      $.ajax({
-        type: 'HEAD',
-        url: '../data/projects.json'
-      }).done(function(xhr, message, data) {
-        var eTag = xhr.getResponseHeaders('etag');
-        if(eTag = localStorage.eTag) {
-          projectView.initIndexPage();
-        } else {
-          Post.loadAll(JSON.parse(localStorage.postRawData));
-        }
-      });
+    if (localStorage.postRawData){
+      // $.ajax({
+      //   type: 'HEAD',
+      //   url: '/data/projects.json'
+      // }).done(function(xhr, message, data) {
+      //   var eTag = xhr.getResponseHeader('eTag');
+      //   if(eTag = localStorage.eTag) {
+      //     projectView.initIndexPage();
+      //   } else {
+      Post.loadAll(JSON.parse(localStorage.postRawData));
+      projectView.initIndexPage();
+        // }
+      // });
     } else {
-      $.getJSON('../data/projects.json', function(data){
+      $.getJSON('/data/projects.json', function(data){
         Post.loadAll(data);
         localStorage.setItem('postRawData', JSON.stringify(data));
         projectView.initIndexPage();
