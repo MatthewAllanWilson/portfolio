df30599c22e2ea47604e6d586c488390bba1a931
MatthewAllanWilson <mattwils@gmail.com>
2016-04-21 14:51:45 -0700

    getting the menu working, so far everything is good except it does not have the titles of projects

--- js/view.js
+++ js/view.js
@@ -6,9 +6,38 @@
     $('.main-nav').on('click', '.tab', function(){
       $('section').hide();
       $('#' + $(this).data('content')).fadeIn();
-      console.log($(this).data('content'));
+      // console.log($(this).data('content'));
     });
   };
+
+  projectView.populateFilter = function() {
+    console.log('populateFilter started running');
+    $('.title').each(function() {
+      console.log('project.each started running');
+      if (!$(this).hasClass('template')) {
+        console.log('!$(this).hasClass started running');
+        var val = $(this).attr('.title').text();
+        console.log('the val variable is ' + val);
+        var optionTag = '<option value="' + val + '">' + val + '</option>';
+        $('#project-filter').append(optionTag);
+        console.log('the optionTag is ' + optionTag);
+      }
+    });
+  };
+  // projectView.populateFilter();
+
+  // articleView.handleAuthorFilter = function() {
+  //   $('#author-filter').on('change', function() {
+  //     if ($(this).val()) {
+  //       $('article').hide();
+  //       $('article[data-author="' + $(this).val() + '"]').fadeIn();
+  //     } else {
+  //       $('article').fadeIn();
+  //       $('article.template').hide();
+  //     }
+  //     $('#category-filter').val('');
+  //   });
+  // };
 
   projectView.initIndexPage = function () {
     Post.all.forEach(function(a){
